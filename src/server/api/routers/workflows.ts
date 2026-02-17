5177e72d5114ab4dacf7474cc45405d0ac613afd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2026-02-17 14:24:43 +0300

    Inngest setup

--- src/server/api/routers/workflows.ts
+++ src/server/api/routers/workflows.ts
@@ -1,3 +1,4 @@
+import { inngest } from "~/inngest/client";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { db } from "~/server/db";
 import { workflowTable } from "~/server/db/schema";
@@ -9,8 +10,13 @@ export const postRouter = createTRPCRouter({
     return await db.select().from(workflowTable).limit(10);
   }),
   createWorkflow: protectedProcedure.mutation(async () => {
-    return await db.insert(workflowTable).values({
-      name: "test-workflow",
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "kennedymungaifmab@gmail.com",
+      },
     });
+
+    return { success: true, message: "Job queued" };
   }),
 });
