8ea5c06a321d8a7f2facce9749dc7084f9c4b241
Kennedy Mungai <kennedymungaifmab@gmail.com>
2026-02-19 10:35:22 +0300

    Set the root route to redirect to the workflows route

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/workflows",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default config;
