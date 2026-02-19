b8a1d368b82dc5c3df851634c6facac16e84f5f2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2026-02-19 11:13:52 +0300

    Made some updates to the app sidebar component

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,6 +1,15 @@
 "use client";
 
 import { FolderOpenIcon } from "lucide-react";
+import Link from "next/link";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "~/components/ui/sidebar";
 
 export const AppSidebar = () => {
   const menuItems = [
@@ -10,5 +19,31 @@ export const AppSidebar = () => {
     },
   ];
 
-  return <div>App Sidebar</div>;
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarContent>
+        {menuItems.map((group) => (
+          <SidebarGroup key={group.title}>
+            <SidebarGroupContent>
+              {group.items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton
+                    tooltip={item.title}
+                    isActive={false}
+                    asChild
+                    className="h-10 gap-x-4 px-4"
+                  >
+                    <Link href={item.url} prefetch>
+                      <item.icon className="size-4" />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+    </Sidebar>
+  );
 };
