416aed5e889b8b7dd594fe44ac6ef1947ebb468f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2026-02-19 11:36:00 +0300

    Added a sidebar footer to the sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,17 +1,31 @@
 "use client";
 
-import { FolderOpenIcon, HistoryIcon, KeyIcon } from "lucide-react";
+import {
+  CreditCardIcon,
+  FolderOpenIcon,
+  HistoryIcon,
+  KeyIcon,
+  StarIcon,
+} from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "~/components/ui/sidebar";
 
 export const AppSidebar = () => {
+  const route = useRouter();
+  const pathname = usePathname();
+
   const menuItems = [
     {
       title: "Main",
@@ -25,6 +39,21 @@ export const AppSidebar = () => {
 
   return (
     <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <SidebarMenuItem>
+          <SidebarMenuButton asChild className="h-10 gap-x-4 px-4">
+            <Link prefetch href={"/"}>
+              <Image
+                src={"/logos/logo.svg"}
+                alt="Nodebase"
+                width={30}
+                height={30}
+              />
+              <span className="text-sm font-semibold">Nodebase</span>
+            </Link>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarHeader>
       <SidebarContent>
         {menuItems.map((group) => (
           <SidebarGroup key={group.title}>
@@ -33,7 +62,11 @@ export const AppSidebar = () => {
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton
                     tooltip={item.title}
-                    isActive={false}
+                    isActive={
+                      item.url === "/"
+                        ? pathname === "/"
+                        : pathname.startsWith(item.url)
+                    }
                     asChild
                     className="h-10 gap-x-4 px-4"
                   >
@@ -48,6 +81,30 @@ export const AppSidebar = () => {
           </SidebarGroup>
         ))}
       </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              tooltip={"Upgrade to Pro"}
+              className="h-10 gap-x-4 px-4"
+              onClick={() => {}}
+            >
+              <StarIcon className="size-4 border-yellow-500 fill-yellow-500" />
+              <span>Upgrade to Pro</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              tooltip={"Billing Portal"}
+              className="h-10 gap-x-4 px-4"
+              onClick={() => {}}
+            >
+              <CreditCardIcon className="size-4" />
+              <span>Billing Portal</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 };
