9ccb913c793475ce273ba3d4247b770ae7e2fc87
Kennedy Mungai <kennedymungaifmab@gmail.com>
2026-02-19 13:52:11 +0300

    Betterauth polars setup

--- src/server/better-auth/config.ts
+++ src/server/better-auth/config.ts
@@ -1,8 +1,9 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-
 import { env } from "~/env";
 import { db } from "~/server/db";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -19,6 +20,25 @@ export const auth = betterAuth({
       redirectURI: "http://localhost:3000/api/auth/callback/github",
     },
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "1a568685-9760-4ff2-9dbb-81d81a9ab329",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL!,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
 
 export type Session = typeof auth.$Infer.Session;
